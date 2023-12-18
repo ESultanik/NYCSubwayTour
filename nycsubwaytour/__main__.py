2aafa80442a3cb4c5752245e019ce39d0643f726
Evan Sultanik <evan@sultanik.com>
2023-12-18 16:03:26 -0500

    More useful printouts

--- nycsubwaytour/__main__.py
+++ nycsubwaytour/__main__.py
@@ -1,4 +1,4 @@
-from .gtfs import Feed
+from .gtfs import Feed, Stop
 from .tour import approximate
 
 
@@ -8,13 +8,15 @@ def main() -> int:
     for stop in [s for s in feed.stops.keys() if s.startswith("S")]:
         del feed.stops[stop]
     solution = approximate(feed)
-    print("\n".join(s.name for s in solution))
+    duration = 0
+    prev_stop: Stop = solution[0]
+    for i, s in enumerate(solution):
+        print(f"{i+1:3d} ({duration / 60 / 60:.1f}hrs):\t{s.name}")
+        if i > 0:
+            duration += feed.shortest_path_lengths[prev_stop.stop_id][s.stop_id]
+        prev_stop = s
     print()
-    min_duration = sum(
-        feed.shortest_path_lengths[s1.stop_id][s2.stop_id]
-        for s1, s2 in zip(solution, solution[1:])
-    )
-    print(f"Minimum expected time: {min_duration / 60 / 60:.1f}hrs")
+    print(f"Minimum expected time: {duration / 60 / 60:.1f}hrs")
     return 0
 
 
